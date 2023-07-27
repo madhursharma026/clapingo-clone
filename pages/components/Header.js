fec9d8ccdf1586d64d84b80ec255c88a98455c2e
madhursharma026 <105696243+madhursharma026@users.noreply.github.com>
2023-07-28 04:56:12 +0530

    Initial Commit

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -12,53 +12,47 @@ export default function Header(props) {
     return (
         <div className={`${styles.headerNavbarContainer} fixed-top`}>
             {/* <Container fluid='xxl'> */}
-                <Navbar expand="lg" className={`${styles.headerNavbar}`}>
-                    <Link className="navbar-brand" href="/">
-                        <img src="https://clapingo.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.700994a7.png&w=48&q=75" alt="#ImgNotFound" />
-                        <span className={styles.websiteLogo}>Clapingo</span>
-                    </Link>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse className="justify-content-end" style={{ background: '#FCFBF4' }}>
-                        <ul className="navbar-nav">
-                            {selectedMenu === 'Home' ?
-                                <li className={`nav-item ${styles.navItems}`} style={{ background: '#EBF6EF', borderRadius: '5px' }}>
-                                    <Link className={`${styles.websiteMenu} nav-link active`} aria-current="page" href="#">Home</Link>
-                                </li>
-                                :
-                                <li className={`nav-item ${styles.navItems}`}>
-                                    <Link className={`${styles.websiteMenu} nav-link active`} aria-current="page" href="/">Home</Link>
-                                </li>
-                            }
+            <Navbar expand="lg" className={`${styles.headerNavbar}`}>
+                <Link className="navbar-brand" href="/">
+                    <img src="https://clapingo.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.700994a7.png&w=48&q=75" alt="#ImgNotFound" />
+                    <span className={styles.websiteLogo}>Clapingo</span>
+                </Link>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse className="justify-content-end" style={{ background: '#FCFBF4' }}>
+                    <ul className="navbar-nav">
+                        {selectedMenu === 'Home' ?
+                            <li className={`nav-item ${styles.navItems}`} style={{ background: '#EBF6EF', borderRadius: '5px' }}>
+                                <Link className={`${styles.websiteMenu} nav-link active`} aria-current="page" href="#">Home</Link>
+                            </li>
+                            :
                             <li className={`nav-item ${styles.navItems}`}>
-                                <span className={`${styles.websiteMenu} nav-link`} onClick={() => moveToPositions()} style={{ cursor: 'pointer' }}>Plans & Pricing</span>
+                                <Link className={`${styles.websiteMenu} nav-link active`} aria-current="page" href="/">Home</Link>
                             </li>
-                            {selectedMenu === 'TeachWithUs' ?
-                                <li className={`nav-item ${styles.navItems}`} style={{ background: '#EBF6EF', borderRadius: '5px' }}>
-                                    <Link className={`${styles.websiteMenu} nav-link`} href="/TeachWithUs">Teach With Us</Link>
-                                </li>
-                                :
-                                <li className={`nav-item ${styles.navItems}`}>
-                                    <Link className={`${styles.websiteMenu} nav-link`} href="/TeachWithUs">Teach With Us</Link>
-                                </li>
-                            }
-                            <li className={`nav-item ${styles.navItems}`}>
-                                <Link className={`${styles.websiteMenu} nav-link`} href="#">For IELTS</Link>
-                            </li>
-                            <li className={`nav-item ${styles.navItems}`}>
-                                <Link className={`${styles.websiteMenu} nav-link`} href="#">Courses</Link>
-                            </li>
-                            <li className={`nav-item ${styles.navItems}`}>
-                                <Link className={`${styles.websiteMenu} nav-link`} href="#">For Organizations <span style={{ fontSize: '12px', color: 'red' }}>New</span></Link>
-                            </li>
-                            <li className={`nav-item ${styles.navItems} mt-2`}>
-                                <button type="button" className={`btn ${styles.btnColorOutline}`}>Login</button>
-                            </li>
-                            <li className={`nav-item ${styles.navItems} mt-2`}>
-                                <button type="button" className={`btn ${styles.btnColorOutline2}`}>Book Your Trial</button>
-                            </li>
-                        </ul>
-                    </Navbar.Collapse>
-                </Navbar>
+                        }
+                        <li className={`nav-item ${styles.navItems}`}>
+                            <span className={`${styles.websiteMenu} nav-link`} onClick={() => moveToPositions()} style={{ cursor: 'pointer' }}>Plans & Pricing</span>
+                        </li>
+                        <li className={`nav-item ${styles.navItems}`}>
+                            <Link className={`${styles.websiteMenu} nav-link`} href="#">For Kids</Link>
+                        </li>
+                        <li className={`nav-item ${styles.navItems}`}>
+                            <Link className={`${styles.websiteMenu} nav-link`} href="#">For IELTS</Link>
+                        </li>
+                        <li className={`nav-item ${styles.navItems}`}>
+                            <Link className={`${styles.websiteMenu} nav-link`} href="#">Courses</Link>
+                        </li>
+                        <li className={`nav-item ${styles.navItems}`}>
+                            <Link className={`${styles.websiteMenu} nav-link`} href="#">For Organizations <span style={{ fontSize: '12px', color: 'red' }}>New</span></Link>
+                        </li>
+                        <li className={`nav-item ${styles.navItems} mt-2`}>
+                            <button type="button" className={`btn ${styles.btnColorOutline}`}>Login</button>
+                        </li>
+                        <li className={`nav-item ${styles.navItems} mt-2`}>
+                            <button type="button" className={`btn ${styles.btnColorOutline2}`}>Book Your Trial</button>
+                        </li>
+                    </ul>
+                </Navbar.Collapse>
+            </Navbar>
             {/* </Container> */}
         </div>
     )
